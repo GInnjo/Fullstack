69ce40690df34c84264814e91b81c896650f6df3
OlVai <olli20005@student.hamk.fi>
2024-04-19 23:45:41 +0300

    Register, User relations to Password and Storage, Deletion of User with its relations.

--- Fullstack/Controllers/RegisterController.cs
+++ Fullstack/Controllers/RegisterController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Fullstack.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Fullstack.Controllers
 {
@@ -7,6 +8,34 @@ namespace Fullstack.Controllers
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> TryRegister(RegisterViewModel form)
+        {
+            if (ModelState.IsValid)
+            {
+                Password password = new Password(form.Password);
+                DatabaseHandler.Save(password);
+                Storage storage = new Storage();
+                DatabaseHandler.Save(storage);
+
+                User user = new User
+                {
+                    FirstName = form.FirstName,
+                    LastName = form.LastName,
+                    Email = form.Email,
+                    CreatedAt = DateTime.Now,
+                    Role = "User",
+                    PasswordId = password.Id,
+                    StorageId = storage.Id
+                };
+                DatabaseHandler.Save(user);
+
+                DatabaseHandler.Delete<User>(user.Id);
+            }
+            return View("Index", form);
         }
     }
 }
---
+++ Fullstack/Models/RegisterViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Fullstack.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        [StringLength(30)]
+        [DisplayName("First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        [DisplayName("Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        [DisplayName("Confirm Password")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
